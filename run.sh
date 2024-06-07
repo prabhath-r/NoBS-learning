154bb362fd8854b5a03f0eef26b6eea05d204d21
prabhath-r <prabhathr96@gmail.com>
2024-06-07 11:20:01 -0400

    fixed run.sh to add instance directory

--- run.sh
+++ run.sh
@@ -8,13 +8,14 @@ export FLASK_ENV=production
 export SECRET_KEY='your_secret_key'
 export DATABASE_URL="sqlite:///$(pwd)/instance/app.db"
 
+
 python3 -m venv nobsenv
 source nobsenv/bin/activate
 
-pip install -r requirements.txt
+pip install -r requirements.txt 
 
-mkdir -p sessions 
+mkdir -p instance sessions ## directory to store the database
 
-python manage_db.py
+python manage_db.py ## initialize the database
 
 gunicorn -w 4 -b 127.0.0.1:8000 app:app  ## Start the server
